d26a6c20b076fda88adf5a8a25e49c97226d790d
inacio88 <inacio88@outlook.com>
2024-11-14 11:25:16 -0300

    master: movie tier

--- ClientApp/src/components/RankItems.js
+++ ClientApp/src/components/RankItems.js
@@ -1,18 +1,25 @@
-import React, { useState, useEffect } from 'react';
-import MovieImageArr from './MovieImages';
-import RankingGrid from './RankingGrid';
-const RankItems = () => {
-    const [items, setItems] = useState([]);
-    const dataType = 1;
+import {useEffect, useState } from 'react';
+import RankingGrid from "./RankingGrid";
+import ItemCollection from "./ItemCollection";
 
-    function drag(ev) {
+const RankItems = ({items, setItems, dataType, imgArr, localStorageKey }) => {
+
+    const [reload, setReload] = useState(false);
+
+    function Reload() {
+        setReload(true);
+    }
+
+    function drag(ev) { 
         ev.dataTransfer.setData("text", ev.target.id);
     }
+
     function allowDrop(ev) {
         ev.preventDefault();
     }
 
     function drop(ev) {
+
         ev.preventDefault();
         const targetElm = ev.target;
         if (targetElm.nodeName === "IMG") {
@@ -21,33 +28,51 @@ const RankItems = () => {
         if (targetElm.childNodes.length === 0) {
             var data = parseInt(ev.dataTransfer.getData("text").substring(5));
             const transformedCollection = items.map((item) => (item.id === parseInt(data)) ?
-            {...item, ranking: parseInt(targetElm.id.substring(5))} : {...item, ranking: item.ranking})
+            { ...item, ranking: parseInt(targetElm.id.substring(5)) } : { ...item, ranking: item.ranking });
+            setItems(transformedCollection);
         }
+
+    }
+    useEffect(() => {
+        if (items == null) {
+            getDataFromApi();
+        }
+
+    }, [dataType]);
+
+    function getDataFromApi() {
+        fetch(`item/${1}`)
+            .then((results) => {
+                return results.json();
+            })
+            .then(data => {
+
+                setItems(data);
+            })
     }
 
     useEffect(() => {
-        fetch(`item/${dataType}`)
-            .then((results) => results.json())
-            .then(data => setItems(data))
-            .catch(error => console.error('Erro ao buscar os dados:', error));
-    }, [dataType]); // Adicione `dataType` como dependência se ele puder mudar
+        if (items != null) {
+            localStorage.setItem(localStorageKey, JSON.stringify(items));
+        }
+        setReload(false);
+    }, [items])
+
+    useEffect(() => {
+        if (reload === true) {
+            getDataFromApi();
+        }
+    },[reload])
+
 
     return (
-
-        <main>
-            <RankingGrid items={items} imgArr={MovieImageArr}/>
-            <div className='items-not-ranked'>
-                {
-                    items.length > 0 ? items.map((item, index) => (
-                        <div className='unranked-cell'>
-
-                            <img id={`item-${item.id}`} src={MovieImageArr.find(o => o.id === item.imageId)?.image} alt='semImagem' />
-                        </div>
-                    )) : <div>Loading...</div>
-                }
-            </div>
-        </main>
-    );
-};
-
+         (items != null)?
+            <main>
+                <RankingGrid items={items} imgArr={imgArr} drag={drag} allowDrop={allowDrop} drop={drop } />
+                <ItemCollection items={items} drag={drag} imgArr={imgArr} />
+                <button onClick={Reload} className="reload" style={{ "marginTop": "10px" }}> <span className="text" >Reload</span > </button>
+            </main>
+            : <main>Loading...</main>
+        )
+}
 export default RankItems;
